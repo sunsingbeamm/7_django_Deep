052cc58f9bdb38b99a7578c6ee05e1d72fd20051
sunsingbeamm <202201616@hufs.ac.kr>
2025-05-11 19:09:38 +0900

    추가해.

---
+++ todo/models.py
@@ -0,0 +1,15 @@
+# todo/models.py
+
+from django.db import models
+
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+    due_date = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+        
+from django.contrib import admin
+admin.site.register(Todo)
